e22dfa9a39ba07ebdc361b5bd696f1b9872f9c08
Brian Gates <brian.gates@theironyard.com>
2017-08-24 11:23:18 -0400

    reset combat rolls after victory/loss

--- src/reducers/rolls.js
+++ src/reducers/rolls.js
@@ -2,6 +2,10 @@ const rolls = (state = {}, action) => {
   switch (action.type) {
     case 'ROLLS':
       return { ...state, [action.phase]: action.rolls }
+    case 'WIN_ATTACK':
+      return { ...state, COMBAT_ROLLS: [] }
+    case 'LOSE_ATTACK':
+      return { ...state, COMBAT_ROLLS: [] }
     default:
       return state
   }
