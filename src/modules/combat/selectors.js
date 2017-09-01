5d287c40c964947cadef81e07f2633d1a09ff55e
Brian Gates <brian.gates@theironyard.com>
2017-09-01 12:33:53 -0400

    add artillery support for infantry

--- src/modules/combat/selectors.js
+++ src/modules/combat/selectors.js
@@ -1,12 +1,37 @@
 import { createSelector } from 'reselect';
-import { combatants } from '../planCombat';
+import { combatants as originalCombatants } from '../planCombat';
 import { getFocusTerritory } from '../../selectors/mergeBoardAndTerritories';
 import { unitCount } from '../../lib/unit';
 export { getFocusTerritory }
 
 export const rollCount = createSelector(
-  combatants,
+  originalCombatants,
   combatants => (combatants.attackers.concat(combatants.defenders)).reduce(unitCount, 0)
+)
+
+const modify = (combatants) => {
+  let { attackers } = combatants
+  const artilleryCount = attackers.filter(u => u.name === 'artillery').reduce((total, unit) => total + unit.ids.length, 0)
+  const infantryCount = attackers.filter(u => u.name === 'infantry').reduce((total, unit) => total + unit.ids.length, 0)
+  if (infantryCount > artilleryCount) {
+    // need separate units
+    const infantry = attackers.find(u => u.name === 'infantry')
+    const supportedIds = infantry.ids.slice(0, artilleryCount)
+    const unsupportedIds = infantry.ids.slice(artilleryCount)
+    const supported = { ...infantry, attack: 2, ids: supportedIds }
+    const unsupported = { ...infantry, ids: unsupportedIds }
+    const newAttackers = [ unsupported, supported, ...attackers.filter(u => u !== infantry) ]
+    return { attackers: newAttackers, defenders: combatants.defenders }
+  } else if (artilleryCount) {
+    const newAttackers = attackers.map(u => u.name === 'infantry' ? { ...u, attack: 2 } : u)
+    return { attackers: newAttackers, defenders: combatants.defenders }
+  }
+  return combatants
+}
+
+export const combatants = createSelector(
+  originalCombatants,
+  combatants => modify(combatants)
 )
 
 export const attackerCasualties = createSelector(
