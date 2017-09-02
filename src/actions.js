1dc496b96d1ab7cb438dc5650ea2bf7a446b37e6
Brian Gates <brian.gates@theironyard.com>
2017-09-02 15:46:49 -0400

    refactor actions

--- src/actions.js
+++ src/actions.js
@@ -1,135 +1,82 @@
-const DEVELOP_TECH = 'DEVELOP_TECH';
-const INCREMENT_RESEARCH = 'INCREMENT_RESEARCH';
-const DECREMENT_RESEARCH = 'DECREMENT_RESEARCH';
-const SET_TECH = 'SET_TECH';
-const ATTEMPT_RESEARCH = 'ATTEMPT_RESEARCH';
-const INCREMENT_PURCHASE = 'INCREMENT_PURCHASE';
-const DECREMENT_PURCHASE = 'DECREMENT_PURCHASE';
-const PLAN_ATTACK = 'PLAN_ATTACK';
-const PLAN_COMBAT = 'PLAN_COMBAT';
-const COMMIT_UNITS = 'COMMIT_UNITS';
-const UNCOMMIT_UNITS = 'UNCOMMIT_UNITS';
-const VIEW_TRANSPORT_LOAD_OPTIONS = 'VIEW_TRANSPORT_LOAD_OPTIONS';
-const LOAD_TRANSPORT = 'LOAD_TRANSPORT';
-const COMMIT_AMPHIB_UNITS = 'COMMIT_AMPHIB_UNITS';
-const UNCOMMIT_AMPHIB_UNITS = 'UNCOMMIT_AMPHIB_UNITS';
-const RESOLVE_COMBAT = 'RESOLVE_COMBAT';
-const ROLLS = 'ROLLS';
-const TOGGLE_CASUALTY = 'TOGGLE_CASUALTY';
-const REMOVE_CASUALTIES = 'REMOVE_CASUALTIES';
-const WIN_ATTACK = 'WIN_ATTACK';
-const LOSE_ATTACK = 'LOSE_ATTACK';
-const PLAN_LAND_PLANES = 'PLAN_LAND_PLANES';
-const SELECT_PLANE_LANDING_OPTION = 'SELECT_PLANE_LANDING_OPTION';
-const SELECT_PLANE_LANDING_TERRITORY = 'SELECT_PLANE_LANDING_TERRITORY';
-const CONFIRM_LAND_PLANES = 'CONFIRM_LAND_PLANES';
-const LAND_PLANES = 'LAND_PLANES';
-const PLAN_MOVEMENT = 'PLAN_MOVEMENT';
-const COMMIT_PLACEMENT = 'COMMIT_PLACEMENT';
-const UNCOMMIT_PLACEMENT = 'UNCOMMIT_PLACEMENT';
-const COMMIT_PLACE_ALL = 'COMMIT_PLACE_ALL';
-const UNCOMMIT_PLACE_ALL = 'UNCOMMIT_PLACE_ALL';
-const PLACE_UNITS = 'PLACE_UNITS';
-const ORDER_UNITS = 'ORDER_UNITS';
-const NEXT_TURN = 'NEXT_TURN';
+export const DEVELOP_TECH = 'DEVELOP_TECH';
+export const INCREMENT_RESEARCH = 'INCREMENT_RESEARCH';
+export const DECREMENT_RESEARCH = 'DECREMENT_RESEARCH';
+export const SET_TECH = 'SET_TECH';
+export const ATTEMPT_RESEARCH = 'ATTEMPT_RESEARCH';
+export const INCREMENT_PURCHASE = 'INCREMENT_PURCHASE';
+export const DECREMENT_PURCHASE = 'DECREMENT_PURCHASE';
+export const PLAN_ATTACK = 'PLAN_ATTACK';
+export const PLAN_COMBAT = 'PLAN_COMBAT';
+export const COMMIT_UNITS = 'COMMIT_UNITS';
+export const UNCOMMIT_UNITS = 'UNCOMMIT_UNITS';
+export const VIEW_TRANSPORT_LOAD_OPTIONS = 'VIEW_TRANSPORT_LOAD_OPTIONS';
+export const LOAD_TRANSPORT = 'LOAD_TRANSPORT';
+export const COMMIT_AMPHIB_UNITS = 'COMMIT_AMPHIB_UNITS';
+export const UNCOMMIT_AMPHIB_UNITS = 'UNCOMMIT_AMPHIB_UNITS';
+export const STRATEGIC_BOMB = 'STRATEGIC_BOMB';
+export const RESOLVE_COMBAT = 'RESOLVE_COMBAT';
+export const ROLLS = 'ROLLS';
+export const TOGGLE_CASUALTY = 'TOGGLE_CASUALTY';
+export const REMOVE_CASUALTIES = 'REMOVE_CASUALTIES';
+export const WIN_ATTACK = 'WIN_ATTACK';
+export const LOSE_ATTACK = 'LOSE_ATTACK';
+export const PLAN_LAND_PLANES = 'PLAN_LAND_PLANES';
+export const SELECT_PLANE_LANDING_OPTION = 'SELECT_PLANE_LANDING_OPTION';
+export const SELECT_PLANE_LANDING_TERRITORY = 'SELECT_PLANE_LANDING_TERRITORY';
+export const CONFIRM_LAND_PLANES = 'CONFIRM_LAND_PLANES';
+export const LAND_PLANES = 'LAND_PLANES';
+export const PLAN_MOVEMENT = 'PLAN_MOVEMENT';
+export const COMMIT_PLACEMENT = 'COMMIT_PLACEMENT';
+export const UNCOMMIT_PLACEMENT = 'UNCOMMIT_PLACEMENT';
+export const COMMIT_PLACE_ALL = 'COMMIT_PLACE_ALL';
+export const UNCOMMIT_PLACE_ALL = 'UNCOMMIT_PLACE_ALL';
+export const PLACE_UNITS = 'PLACE_UNITS';
+export const ORDER_UNITS = 'ORDER_UNITS';
+export const NEXT_TURN = 'NEXT_TURN';
 
-export const resolveCombat = (territory) => {
-  return { type: RESOLVE_COMBAT, territory }
-}
+export const resolveCombat = (territory) => (  
+  { type: RESOLVE_COMBAT, territory }
+)
 
-const removeCasualties = (defenderCasualties, territoryIndex, currentPower) => {
-  return {
+export const strategicBomb = (territory) => (  
+  { type: STRATEGIC_BOMB, territory }
+)
+
+export const removeCasualties = (defenderCasualties, territoryIndex, currentPower) => ( 
+  {
     type: REMOVE_CASUALTIES,
     defenderCasualties,
     territoryIndex,
     currentPower
   }
-}
+)
 
-const roll = (phase, rolls) => {
-  return {
+export const roll = (phase, rolls) => ( 
+  {
     type: ROLLS,
     phase,
     rolls
   }
-}
+)
 
-const planAttack = (territory) => {
-  return {
-    type: PLAN_ATTACK,
-    territory
-  }
-}
+export const planAttack = (territory) => { type: PLAN_ATTACK, territory }
 
-const planLandPlanes = (territory) => {
-  return {
+export const planLandPlanes = (territory) => ( 
+  {
     type: PLAN_LAND_PLANES,
     territory
   }
-}
+)
 
-const winAttack = (territoryIndex, currentPower) => {
-  return { 
+export const winAttack = (territoryIndex, currentPower) => ( 
+  { 
     type: WIN_ATTACK, 
     territoryIndex,
     currentPower
   }
-}
+ )
 
-const planMovement = (territory) => {
-  return {
-    type: PLAN_MOVEMENT,
-    territory
-  }
-}
+export const planMovement = (territory) => { type: PLAN_MOVEMENT, territory }
 
-const orderUnits = (territory) => {
-  return {
-    type: ORDER_UNITS,
-    territory
-  }
-}
+export const orderUnits = (territory) => { type: ORDER_UNITS, territory }
 
-
-export {
-  DEVELOP_TECH,
-  INCREMENT_RESEARCH,
-  DECREMENT_RESEARCH,
-  SET_TECH,
-  ATTEMPT_RESEARCH,
-  INCREMENT_PURCHASE,
-  DECREMENT_PURCHASE,
-  PLAN_ATTACK,
-  PLAN_COMBAT,
-  COMMIT_UNITS,
-  UNCOMMIT_UNITS,
-  VIEW_TRANSPORT_LOAD_OPTIONS,
-  LOAD_TRANSPORT,
-  COMMIT_AMPHIB_UNITS,
-  UNCOMMIT_AMPHIB_UNITS,
-  RESOLVE_COMBAT,
-  ROLLS,
-  TOGGLE_CASUALTY,
-  REMOVE_CASUALTIES,
-  WIN_ATTACK,
-  LOSE_ATTACK,
-  PLAN_LAND_PLANES,
-  SELECT_PLANE_LANDING_OPTION,
-  SELECT_PLANE_LANDING_TERRITORY,
-  CONFIRM_LAND_PLANES,
-  LAND_PLANES,
-  COMMIT_PLACEMENT,
-  UNCOMMIT_PLACEMENT,
-  COMMIT_PLACE_ALL,
-  UNCOMMIT_PLACE_ALL,
-  PLACE_UNITS,
-  ORDER_UNITS,
-  NEXT_TURN,
-  roll,
-  removeCasualties,
-  planAttack,
-  winAttack,
-  planMovement,
-  planLandPlanes,
-  orderUnits
-}
