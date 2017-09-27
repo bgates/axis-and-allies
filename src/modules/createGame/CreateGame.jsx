9bb327b4b26468b523753668b9e9fb6fb3f27ebc
Brian Gates <brian.gates@theironyard.com>
2017-09-26 21:17:05 -0400

    progress on game creation in firebase

--- src/modules/createGame/CreateGame.jsx
+++ src/modules/createGame/CreateGame.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import ReactTooltip from 'react-tooltip'
+import '../../assets/styles/login.css'
 
 class CreateGame extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = { select: '' }
+    this.setSelect = this.setSelect.bind(this)
+  }
 
   createGame (event) {
     event.preventDefault()
@@ -10,51 +16,63 @@ class CreateGame extends Component {
     this.props.createGame(data)
   }
 
+  setSelect (event) {
+    this.setState({ select: event.target.value })
+  }
+
   render () {
     return (
-      <div>
-        <a data-tip className="help">?</a>
+      <div className="create-game">
         <h1>Create Game</h1>
-        <ReactTooltip place="bottom">
-          <p><strong>Login to your account.</strong> If you are already playing a single game in progress, you will automatically join that game. If you are playing multiple games, you will be given the option of choosing which one to join. If you are not part of a current game, you will be taken to a dialog to let you create one.</p>
-        </ReactTooltip>
+        <p>Enter the email addresses and screen names for everyone who wants to play the game. You can enter an email address more than once if you are playing with fewer than seven people.</p>
+        <p>If you are already playing a single game in progress, you will automatically join that game. If you are playing multiple games, you will be given the option of choosing which one to join. If you are not part of a current game, you will be taken to a dialog to let you create one.</p>
         <form onSubmit={this.createGame.bind(this)}>
           <div>
             <label>Germany</label>
-            <input name="email_germany" />
-            <input name="screenname_germany" />
+            <input name="email_germany" placeholder="Email for German player"/>
+            <input name="screenname_germany" placeholder="German screen name" />
           </div>
           <div>
             <label>USSR</label>
-            <input name="email_ussr" />
-            <input name="screenname_ussr" />
+            <input name="email_ussr" placeholder="Email for Soviet player"/>
+            <input name="screenname_ussr" placeholder="Soviet screen name"/>
           </div>
           <div>
             <label>Japan</label>
-            <input name="email_japan" />
-            <input name="screenname_japan" />
+            <input name="email_japan" placeholder="Email for Japanese player"/>
+            <input name="screenname_japan" placeholder="Japanese screen name"/>
           </div>
           <div>
             <label>UK</label>
-            <input name="email_uk" />
-            <input name="screenname_uk" />
+            <input name="email_uk" placeholder="Email for British player"/>
+            <input name="screenname_uk" placeholder="British screen name"/>
           </div>
           <div>
             <label>Italy</label>
-            <input name="email_italy" />
-            <input name="screenname_italy" />
+            <input name="email_italy" placeholder="Email for Italian player"/>
+            <input name="screenname_italy" placeholder="Italian screen name" />
           </div>
           <div>
             <label>China</label>
-            <input name="email_china" />
-            <input name="screenname_china" />
+            <input name="email_china" placeholder="Email for Chinese player"/>
+            <input name="screenname_china" placeholder="Chinese screen name"/>
           </div>
           <div>
             <label>USA</label>
-            <input name="email_us" />
-            <input name="screenname_us" />
+            <input name="email_us" placeholder="Email for American player"/>
+            <input name="screenname_us" placeholder="American screen name"/>
           </div>
-          <button>Create Game</button>
+          <select onChange={this.setSelect}>
+            <option>Who are you playing?</option>
+            <option value="germany">Germany</option>
+            <option value="ussr">USSR</option>
+            <option value="japan">Japan</option>
+            <option value="uk">United Kingdom</option>
+            <option value="italy">Italy</option>
+            <option value="china">China</option>
+            <option value="us">USA</option>
+          </select>
+          <button disabled={!this.state.select}>Create Game</button>
         </form>
       </div>
     )
--- src/modules/createGame/CreateGameContainer.js
+++ src/modules/createGame/CreateGameContainer.js
@@ -10,14 +10,33 @@ const createGame = (formData, firebase) => {
     let game = { powers: [ ...initialPowers] }
     for (const pair of formData.entries()) {
       const [attr, name] = pair[0].split('_')
-      console.log(name)
       let power = game.powers.find(p => p.name.toLowerCase() === name)
       power[attr] = pair[1]
     }
     game.currentPlayer = game.powers[0].email
-    firebase.push('/games', game).then(snapshot => console.log(snapshot.key))
+    firebase.push('/games', game)
+      .then(snapshot => connectEachPlayerToGame(firebase, game, snapshot.key))
     //firebase.login({ email, password }).then(() => dispatch(push('/')))
   }
+}
+
+const emailToKey = emailAddress => btoa(emailAddress)
+const keyToEmail = key => atob(key)
+
+const connectEachPlayerToGame = (firebase, game, gameId) => {
+  firebase.ref('/emails').once('value').then(snapshot => {
+    const emails = snapshot.val()
+    console.log('emails', emails)
+    game.powers.forEach(power => {
+      const emailKey = emailToKey(power.email)
+      let uid = emails[emailKey]
+      if (!uid) {
+        uid = emailKey
+        firebase.update('/invites', { [emailKey]: true })
+      }
+      firebase.update(`/users/${uid}/${gameId}`, { [power.name]: power.screenname })
+    })
+  })
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
