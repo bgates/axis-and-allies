83a5f030e426959c894b6aa844b6135e7da3ef5b
Brian Gates <brian.gates@theironyard.com>
2017-10-02 22:49:09 -0400

    show chat only to logged in user

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import { compose } from 'redux'
+import { connect } from 'react-redux'
+import {
+  firebaseConnect,
+  isEmpty
+} from 'react-redux-firebase'
 import { BoardContainer } from './modules/board'
 import Chat from './modules/chat'
+import { LoginContainer } from './modules/login'
 import { CreateGameContainer } from './modules/createGame'
 
-const Game = () => (    
+const Game = ({ authError, auth, profile }) => (    
   <div>
     <BoardContainer />
-    <Chat />
+    {!isEmpty(auth) ? <Chat profile={profile} /> : <LoginContainer />}
   </div>
 )
+
+const GameContainer = compose(firebaseConnect(),
+  connect(
+    ({ firebase: { authError, auth, profile } }) => ({ 
+      authError, auth, profile
+    })
+  )
+)(Game)
 
 const App = () => ( 
   <Switch>
     <Route path='/create-game' component={CreateGameContainer} />
-    <Route component={Game} />
+    <Route component={GameContainer} />
   </Switch>
 )
 
