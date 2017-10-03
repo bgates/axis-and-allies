cf754d2e6027be3e2a8405ee97cd055ed466958a
Brian Gates <brian.gates@theironyard.com>
2017-10-03 09:18:58 -0400

    refactor chat appearance

--- src/modules/chat/Chat.jsx
+++ src/modules/chat/Chat.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { firebaseConnect, isLoaded, isEmpty } from 'react-redux-firebase'
+import '../../assets/styles/chat.css'
 
-class Chat extends Component {
+class Messages extends Component {
 
-  handleAdd (chat, gameId) {
-    const msg = this[chat]
-    this.props.firebase.push(`/${chat}/${gameId}`, { text: msg.value })
+  handleAdd () {
+    const msg = this.msg
+    this.props.firebase.push(this.props.node, { text: msg.value })
     msg.value = ''
   }
 
@@ -22,23 +23,36 @@ class Chat extends Component {
   }
 
   render () {
-    const { chat, sideChat, profile } = this.props 
+    const { type, list } = this.props
+    return (
+      <section>
+        <h2>{type} Messages</h2>
+        <ul>{this.messageList(list)}</ul>
+        <input type="text" ref={node => this.msg = node} />
+        <button onClick={() => this.handleAdd()}>Add</button>
+      </section>
+    )
+  }
+}
+
+class Chat extends Component {
+
+  render () {
+    const { chat, sideChat, profile, firebase } = this.props 
     const { axis, currentGameId }= profile
     const side = axis ? 'axisChat' : 'alliesChat'
     return (
-      <section id="chat">
-        <section id="public">
-          <h2>Public Messages</h2>
-          <ul>{this.messageList(chat)}</ul>
-          <input type="text" ref={node => this.chat = node} />
-          <button onClick={() => this.handleAdd('chat', currentGameId)}>Add</button>
-        </section>
-        <section id="private">
-          <h2>Private Messages</h2>
-          <ul>{this.messageList(sideChat)}</ul>
-          <input type="text" ref={node => this[side] = node} />
-          <button onClick={() => this.handleAdd(side, currentGameId)}>Add</button>
-        </section>
+      <section className="chat">
+        <Messages 
+          type="Public" 
+          list={chat} 
+          firebase={firebase}
+          node={`/chat/${currentGameId}`} />
+        <Messages 
+          type="Private" 
+          list={sideChat} 
+          firebase={firebase}
+          node={`/${side}/${currentGameId}`} />
       </section>
     )
   }
