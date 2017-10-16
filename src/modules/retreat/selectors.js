8daddb6e6980cc662650e7b0db65ecbb93897c75
Brian Gates <brian.gates@theironyard.com>
2017-10-16 09:02:43 -0400

    retreat only to adjacent territory

--- src/modules/retreat/selectors.js
+++ src/modules/retreat/selectors.js
@@ -1,19 +1,22 @@
 import { createSelector } from 'reselect';
-import { mergeBoardAndTerritories } from '../../selectors/mergeBoardAndTerritories'
+import { mergeBoardAndTerritories, getFocusTerritory } from '../../selectors/mergeBoardAndTerritories'
 import { combatants } from '../planCombat'
 import unitTypes from '../../config/unitTypes'
 
-const _retreatOptions = (attackers, territories) => {
+const _retreatOptions = (attackers, territory, territories) => {
   if (attackers.every(attacker => attacker.air)) {
     // TODO: air retreat - all friendly territories every unit can reach
   } else {
-    const retreatOptionIndexes = attackers.reduce((options, attacker) => options.concat(attacker.originIndexes), [])
-    return territories.filter(territory => retreatOptionIndexes.includes(territory.index))
+    const retreatOptionIndexes = attackers.reduce((options, attacker) => (
+      options.concat(attacker.originIndexes || attacker.originIndex)
+    ), []).filter(option => territory.adjacentIndexes.includes(option))
+    return territories.filter(_territory => retreatOptionIndexes.includes(_territory.index))
   }
 }
   
 export const retreatOptions = createSelector(
   mergeBoardAndTerritories,
+  getFocusTerritory,
   combatants,
-  (territories, { attackers }) => _retreatOptions(attackers, territories)
+  (territories, territory, { attackers }) => _retreatOptions(attackers, territory, territories)
 )
