93afbe2b4af817cea4bfa62257aae259692e9e07
Brian Gates <brian.gates@theironyard.com>
2017-10-30 00:05:41 -0400

    use new income action

--- src/modules/income/reducer.js
+++ src/modules/income/reducer.js
@@ -1,16 +1,17 @@
-import { currentPowerNPL } from './selectors'
+import { SET_INCOME } from '../../actions'
 
 const income = (state = 0, action) => {
   switch (action.type) {
     case '@@router/LOCATION_CHANGE': 
       const { pathname } = action.payload.location
-      if (pathname === '/income' && !state) {
-        return currentPowerNPL(state)
-      } else if (pathname === '/confirm-finish') {
+      if (pathname === '/confirm-finish') {
         return 0
       } else {
         return state
       }
+    case SET_INCOME: {
+      return action.amount
+    }
     default: 
       return state
   }
