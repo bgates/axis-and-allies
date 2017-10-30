17d1d1dbf7e23dc9c91bbc3f9e868d52fb776dbb
Brian Gates <brian.gates@theironyard.com>
2017-10-29 23:46:40 -0400

    save before destroying incomeHelper

--- src/modules/board/reducer/incomeHelper.js
+++ src/modules/board/reducer/incomeHelper.js
@@ -1,11 +1,11 @@
-import { currentPowerNPL } from '../../income';
+import { currentPowerNPL } from '../../income'
 
 const incomeHelper = (state, action) => {
   switch (action.type) {
     case '@@router/LOCATION_CHANGE': 
-      const { pathname } = action.payload.location;
+      const { pathname } = action.payload.location
       if (pathname === '/income' && !state.currentPowerIncome) {
-        return currentPowerNPL({ board: state })
+        return currentPowerNPL(state)
       } else if (pathname === '/confirm-finish') {
         return 0
       } else {
