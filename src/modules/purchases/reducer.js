9a0c961f935f19b30c3f3fd1c707b6bf078ad48b
Brian Gates <brian.gates@theironyard.com>
2017-11-21 12:33:30 -0500

    remove purchases decremented to zero

--- src/modules/purchases/reducer.js
+++ src/modules/purchases/reducer.js
@@ -1,15 +1,17 @@
+import { omit } from 'ramda'
 import { 
   INCREMENT_PURCHASE, 
   DECREMENT_PURCHASE, 
   NEXT_TURN 
-} from '../../actions';
+} from '../../actions'
   
 const origin = {}
 
 const purchases = (state = origin, action) => {
-  switch (action.type) {
+  const { type, unit } = action
+  switch (type) {
     case INCREMENT_PURCHASE: {
-      const unitName = action.unit.name
+      const unitName = unit.name
       if (state[unitName]) {
         return { ...state, [unitName]: state[unitName] + 1}
       } else {
@@ -17,8 +19,12 @@ const purchases = (state = origin, action) => {
       }
     }
     case DECREMENT_PURCHASE: {
-      const unitName = action.unit.name
-      return { ...state, [unitName]: state[unitName] - 1 }
+      const unitName = unit.name
+      if (state[unitName] === 1) {
+        return omit(unitName, state)
+      } else {
+        return { ...state, [unitName]: state[unitName] - 1 }
+      }
     }
     case NEXT_TURN:
       return origin
