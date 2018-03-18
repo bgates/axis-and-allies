d11d5b5058ebc5159fb65141c8451aa10a340f55
Brian Gates <briandotgates@gmail.com>
2018-03-18 00:45:11 -0400

    restore wrapper div for css

--- src/components/Forces.jsx
+++ src/components/Forces.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { UnitImg } from './UnitFigure'
 
 const Forces = ({ units, classNameFct, handleClick }) => (
-  units.map(({ id, power, type }) => (
-    <UnitImg 
-      key={id} 
-      power={power} 
-      className={classNameFct(id)}
-      handleClick={handleClick(id, type)}
-      name={type} />
-  ))
+  <div>
+    {units.map(({ id, power, type })=> (
+      <UnitImg 
+        key={id} 
+        power={power} 
+        className={classNameFct(id)}
+        handleClick={handleClick(id, type)}
+        name={type} />
+      ))
+    }
+  </div>
 )
 
 Forces.defaultProps = {
