4ea4af99f63917008bdb593740ad790050ca3a6d
Brian Gates <briandotgates@gmail.com>
2018-07-08 12:23:09 -0400

    make it easier to fake die rolls

--- src/lib/numericalDieRolls.js
+++ src/lib/numericalDieRolls.js
@@ -1,6 +1,13 @@
 import { times } from 'ramda'
 
+let fakeRolls = [
+  2, 3, 4, 5, 5, 1
+]
+
+const rollFunction = () => (
+  Math.ceil(Math.random() * 6) // fakeRolls.shift()
+)
 const dieRolls = (n) => {
-  return times(() => Math.ceil(Math.random() * 6), n)
+  return times(rollFunction, n)
 }
 export default dieRolls
