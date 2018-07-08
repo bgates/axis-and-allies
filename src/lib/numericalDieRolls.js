2bf7e906e6ead6d22656eb7fea0c1e1b9c896ef3
Brian Gates <briandotgates@gmail.com>
2018-07-08 15:03:49 -0400

    provide generator for die rolls

--- src/lib/numericalDieRolls.js
+++ src/lib/numericalDieRolls.js
@@ -1,11 +1,20 @@
 import { times } from 'ramda'
 
-let fakeRolls = [
-  2, 3, 4, 5, 5, 1
-]
+function* rollMaker() {
+  let fakeRolls = [
+    2, 3, 4, 5, 5, 1
+  ]
+  while (true) {
+    yield fakeRolls.length ? fakeRolls.shift() : dieRoll(6)
+  }
+}
 
+const dieRoll = (n) => Math.ceil(Math.random() * n)
+
+const rollGenerator = rollMaker()
+const random = false
 const rollFunction = () => (
-  Math.ceil(Math.random() * 6) // fakeRolls.shift()
+  random ? dieRoll(6) : rollGenerator.next().value
 )
 const dieRolls = (n) => {
   return times(rollFunction, n)
